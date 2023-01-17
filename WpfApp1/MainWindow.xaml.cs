b955103e182e507f1c8bc539d48ff4208153ce20
user228 <forfarmcard@mail.ru>
2023-01-17 15:30:55 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tbResult = tbResult;
+            Model.tbSignText = tbSign;
+   
+            cmbOperation.ItemsSource = Model.cmbOperation;
+            cmbOperation.SelectedIndex = 0;
+
+        }
+
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+            Model.tbFirst = tbFirst.Text;
+            Model.tbSecond = tbSecond.Text;
+            Model.tbOperationResult = cmbOperation.SelectedIndex;
+        }
+
+        private void cmbOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.tbSignOperation = cmbOperation.SelectedIndex;
+        }
+    }
+}
---
+++ WpfApp1/Model.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public static class Model
+    {
+
+        public static string tbFirst;
+        public static string tbSecond;
+
+        public static List<string> cmbOperation = new List<string>() { "Сложение", "Умножение", "Деление", "Вычитание" };
+        public static List<string> tbSign = new List<string>() { "+", "*", "/", "-" };
+
+
+        public static TextBlock tbSignText = new TextBlock();
+        public static TextBlock tbResult = new TextBlock();
+
+
+        public static int tbSignOperation
+        {
+            set => tbSignText.Text = tbSign[value];
+
+
+
+        }
+        public static int tbOperationResult
+        {
+            set
+            {
+                try
+                {
+                    double firstVariable = Convert.ToDouble(tbFirst);
+                    double secondVariable = Convert.ToDouble(tbSecond);
+
+                    switch (value)
+                    {
+                        case 0:
+                            tbResult.Text = Convert.ToString(firstVariable + secondVariable);
+
+                            break;
+                        case 1:
+                            tbResult.Text = Convert.ToString(firstVariable * secondVariable);
+
+                            break;
+                        case 2:
+                            tbResult.Text = Convert.ToString(firstVariable / secondVariable);
+
+                            break;
+                        case 3:
+                            tbResult.Text = Convert.ToString(firstVariable - secondVariable);
+
+                            break;
+
+
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Что - то пошло не так", "Попробуйте еще раз", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+
+            }
+        }
+    }
+}
